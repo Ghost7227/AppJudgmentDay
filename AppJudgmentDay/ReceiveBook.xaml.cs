54f090e4512bbb8bfe2b8f3306e30476cec36012
Ghost7227 <ivanvadimovichkovylin@gmail.com>
2024-12-26 20:11:13 +0300

    Исправлен вывод книг пользователя в меня возврата книг

--- AppJudgmentDay/ReceiveBook.xaml.cs
+++ AppJudgmentDay/ReceiveBook.xaml.cs
@@ -52,11 +52,6 @@ namespace AppJudgmentDay
             }
         }
 
-        private void booksListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
@@ -72,6 +67,7 @@ namespace AppJudgmentDay
                 //chooseBook.Add(selectedItem);
                 worker.IsEnabled = false;
                 MessageBox.Show(selectedItem);
+                booksList.IsEnabled = true;
                 Searchbooks();
             }
         }
@@ -89,15 +85,21 @@ namespace AppJudgmentDay
         List<int> indexs = new List<int>();
         private void Searchbooks()
         {
+            string[] readerArr = reader1.Split(',');
+            string readerObrez = $"{readerArr[0]}{readerArr[1]}";
+            MessageBox.Show(readerObrez);
             try
             {
+                //MessageBox.Show(reader1);
                 var lines = File.ReadAllLines(BookPath);
+                //string strlin = $"{lines[0]} ПРОБЕЛЫ {lines[1]} ПРОБЕЛЫ {lines[2]}";
+                //MessageBox.Show(strlin);
                 for (int i = 0; i < lines.Length; i++)
                 {
-                    if (lines[i].Contains(reader1))
+                    if (lines[i].Contains(readerObrez))
                     {
                         indexs.Add(i);
-                        MessageBox.Show(i.ToString());
+                        LoadBooks();
                     }
                 }
             }
@@ -105,9 +107,9 @@ namespace AppJudgmentDay
             {
                 MessageBox.Show("Fatality!", ex.Message);
             }
-            LoadBooks();
-        }
 
+        }
+        //string path, List<int> indexs
         private void LoadBooks()
         {
             try
@@ -120,7 +122,9 @@ namespace AppJudgmentDay
                     if (index >= 0 && index < lines.Length)
                     {
                         string line = lines[index];
-                        booksList.Items.Add(line);
+                        string[] splitLine = line.Split('#');
+                        string lineout = $"{splitLine[0]} {splitLine[1]} {splitLine[2]} {splitLine[3]} {splitLine[4]} {splitLine[5]}";
+                        booksList.Items.Add(lineout);
                     } 
                 }
             }
@@ -129,7 +133,10 @@ namespace AppJudgmentDay
                 MessageBox.Show("Ошибка чтения или вывода книг");
             }
         }
+        private void booksListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
+        }
 
     }
 }
